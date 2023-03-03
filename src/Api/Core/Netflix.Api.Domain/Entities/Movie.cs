f2a2c852e90619e26b9821f5dfb709365d6969db
baran <byasarn@gmail.com>
2023-03-03 09:09:33 +0300

    Project Folders

---
+++ src/Api/Core/Netflix.Api.Domain/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix.Api.Domain.Entities
+{
+    public class Movie:BaseEntity
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public TimeSpan Duration { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        public ICollection<MovieComment> MovieComments { get; set; }
+
+    }
+}
