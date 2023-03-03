fae75b829d20528b24052d0408e5d3614163e8ef
baran <byasarn@gmail.com>
2023-03-03 21:28:42 +0300

    mvc

---
+++ src/Clients/Netlflix.Client.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Netlflix.Client.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
